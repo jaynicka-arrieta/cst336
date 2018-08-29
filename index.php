02b1c96bc88e626ba2edb7a20d4a82199efa6bc3
Jaynicka Arrieta <jarrieta@csumb.edu>
2018-08-29 21:38:23 +0000

    fixed index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
-<html><body>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <h1>Hello world!</h1>
+    </head>
+    
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo <h1>"Welcome to my site!"</h1>;
+echo "<h1>Welcome to my site!</h1>";
 
 ?>
 </body>
