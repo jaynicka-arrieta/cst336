36436ab6f9aa5a5361aff09a026daf323e56e2d7
Jaynicka Arrieta <jarrieta@csumb.edu>
2018-09-12 22:27:29 +0000

    added lab 2

---
+++ labs/777/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <?php 
+        
+    include 'inc/functions.php';
+        
+    ?>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+    <div id="main">
+        
+        <?php
+            play();
+        ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        
+        <style>
+            @import url("css/styles.css");
+        </style>
+        
+    </div>
+        
+    </body>
+    
+</html>
