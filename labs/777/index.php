5ddc63bc6d53bc755f857a0f29b14438bb7c3760
Jaynicka Arrieta <jarrieta@csumb.edu>
2018-09-19 21:10:54 +0000

    added footer

--- labs/777/index.php
+++ labs/777/index.php
@@ -23,7 +23,28 @@
             @import url("css/styles.css");
         </style>
         
+        <br/><br/>
+        
     </div>
+    
+    <footer>
+            
+        <div>
+          
+          <hr>
+          <h5>
+            CST 336 - Internet Programming 2017&copy; Arrieta <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitious. <br />
+            It is used for academic purposes
+          </h5>
+            
+        </div>
+  
+        <figure>
+          <img src="img/csumb.jpg" alt="CSUMB logo" />
+        </figure>
+  
+      </footer>
         
     </body>
     
