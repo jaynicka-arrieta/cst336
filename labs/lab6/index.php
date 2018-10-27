bfc06d1b5761e06d687dd0d1a801ea04e3a0d49e
Jaynicka Arrieta <jarrieta@csumb.edu>
2018-10-27 22:46:57 +0000

    added lab 6 and dbConnection

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -1,105 +1,139 @@
-<?php 
-    include 'dbConnection.php';
-    $conn = getDatabaseConnection("ottermart");
+<?php
+
+include '../../inc/dbConnection.php';
+$dbConn = startConnection("ottermart");
+
+function displayCategories() { 
+    global $dbConn;
     
-    function displayCategories() {
-        global $conn;
-        $sql = "SELECT catID, catName from om_category ORDER BY catName";
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        foreach ($records as $record) {
-            echo "<option value = '". $record["catId"] . "' >" . $record["catName"] . "</option>";
-        }
+    $sql = "SELECT * FROM om_category ORDER BY catName";
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //print_r($records);
+    //echo "<hr>";
+    //echo $records[2] . "<br>";
+    //echo $records[1]['catDescription'] . "<br>";
+    
+    foreach ($records as $record) {
+        echo "<option value='".$record['catId']."'>" . $record['catName'] . "</option>";
+    }
+}
+
+function filterProducts() {
+    global $dbConn;
+    
+    $namedParameters = array();
+    $product = $_GET['productName'];
+    
+    //This SQL works but it doesn't prevent SQL INJECTION (due to the single quotes)
+    //$sql = "SELECT * FROM om_product
+    //        WHERE productName LIKE '%$product%'";
+  
+    $sql = "SELECT * FROM om_product WHERE 1"; //Gettting all records from database
+    
+    if (!empty($product)){
+        //This SQL prevents SQL INJECTION by using a named parameter
+         $sql .=  " AND productName LIKE :product OR productDescription LIKE :product";
+         $namedParameters[':product'] = "%$product%";
     }
     
-    function displaySearchResults() {
-        global $conn;
-        
-        if (isset($_GET['searchForm'])) {
-            echo "<h3>Products Found: </h3>";
-            $namedParameters = array();
-            
-            $sql = "SELECT * FROM om_product WHERE 1";
-            
-            if (!empty($_GET['product'])) {
-                $sql .= " AND productName LIKE :productName";
-                // .= means that it will add to the end of the query already in $sql 
-                $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
-                //when using Named Parameters you are generally saving the information from the form into an array
-            }
-            
-            if (!empty($_GET['category'])) {
-                $sql .= " AND catId LIKE :categoryId";
-                $namedParameters[":categoryId"] = $_GET['category'];
-            }
-            
-            if (!empty($_GET['priceFrom'])) {
-                $sql .= " AND price >= :priceFrom";
-                $namedParameters[":priceFrom"] = $_GET['priceFrom'];
-            }
-            
-            if (!empty($_GET['priceTo'])) {
-                $sql .= " AND price <= :priceTo";
-                $namedParameters[":priceTo"] = $_GET['priceTo'];
-            }
-            
-            if (isset($_GET['orderBy'])) {
-                if ($GET['orderBy'] == "price") {
-                    $sql .= " ORDER BY price";
-                } else {
-                    $sql .= " ORDER BY productName";
-                }
-            }
-            
-            $stmt = $conn->prepare($sql);
-            $stmt->execute($namedParameters);
-            $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
-            foreach($records as $record) {
-                echo $record["productName"] . " " . $record["productDescription"] . " $" . $record["price"] . "</br></br>";
-            }
-        }
+    if (!empty($_GET['category'])){
+        //This SQL prevents SQL INJECTION by using a named parameter
+         $sql .=  " AND catId =  :category";
+          $namedParameters[':category'] = $_GET['category'] ;
     }
     
+    if(!empty($_GET['priceFrom'])) {
+        $sql .= " AND price >= :priceFrom";
+        $namedParameters["priceFrom"] = $_GET['priceFrom'];
+    }
+    
+    if(!empty($_GET['priceTo'])) {
+        $sql .= " AND price <= :priceTo";
+        $namedParameters["priceTo"] = $_GET['priceTo'];
+    }
+    
+    //echo $sql;
+    
+    if (isset($_GET['orderBy'])) {
+        
+        if ($_GET['orderBy'] == "productPrice") {
+            
+            $sql .= " ORDER BY price";
+        } else {
+            
+              $sql .= " ORDER BY productName";
+        }
+        
+        
+    }
+
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+
+    foreach ($records as $record) {
+        
+        echo "<a href='purchaseHistory.php?productId=".$record['productId']."'>";
+        echo $record['productName'];
+        echo "</a> ";
+        echo $record['productDescription'] . " $" .  $record['price'] .   "</br></br>";   
+        
+    }
+
+
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>OtterMart Product Search</title>
-        <link href = "css/styles.css" rel = "stylesheet" type = "text/css" />
+        <title>Ottermart Product Search</title>
+        <link rel='stylesheet' href='css/styles.css' type='text/css' />
     </head>
-    
     <body>
         
-        <div>
-            <h1>OtterMart Product Search</h1>
-            
+        <header>
+            <h1>Ottermart</h1>
+            <h2>Product Search</h2>
+        </header>
+        
+        <center><div id="dv">
             <form>
-                Product: <input type = "text" name = "product"/>
-                </br></br>
-                Category: 
-                    <select name = "category">
-                        <option value = "">Select One</option>
-                        <?php displayCategories(); ?>
-                    </select>
-                </br></br>
-                Price: From <input type = "text" name = "priceFrom" size = "7"/>
-                       To <input type = "text" name = "priceTo" size = "7"/>
-                </br></br>
-                Order results by: </br>
-                <input type = "radio" name = "orderBy" value = "price" /> Price </br>
-                <input type = "radio" name = "orderBy" value = "name" /> Name 
                 
+                <b>Product:</b> <input type="text" name="productName" placeholder="Product keyword" /> </br></br>
+                
+                <b>Category:</b> 
+                <select name="category">
+                   <option value=""> Select one </option>  
+                   <?=displayCategories()?>
+                </select>
                 </br></br>
-                <input type = "submit" value = "Search" name = "searchForm" />
+                
+                <b>Price:</b> From: <input type="text" name="priceFrom" size="7"  /> 
+                 To: <input type="text" name="priceTo" size="7" />
+                </br></br>
+                <b>Order By:</b>
+                Price <input type="radio" name="orderBy" value="productPrice">
+                Name <input type="radio" name="orderBy" value="productName">
+                </br></br>
+                <input type="submit" name="submit" value="Search!"/>
             </form>
+            </br></br>
+            <hr>
             
-            </br>
-        </div>
-        <hr>
-        <?php displaySearchResults(); ?>
+            <div id="results">
+            <?php
+                if($_GET['submit'] == "Search!") {
+                    echo "<h2> Results: </h2>";
+                    filterProducts();
+                }
+            ?>
+            </div>
+        </div></center>
+
+
     </body>
 </html>
