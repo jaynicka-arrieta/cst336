b00b621540cef84b940a57cbc912bc177b41c289
Jaynicka Arrieta <jarrieta@csumb.edu>
2018-12-08 00:02:10 +0000

    added lab10

---
+++ labs/lab10/inc/footer.php
@@ -0,0 +1,9 @@
+<hr>
+        <footer>
+            
+            Disclaimer: The information on this site is fake.<br>
+            &copy; Arrieta 2018
+            
+        </footer>
+        
+        
